504e85f979063a2b5d434e4908b0c1e309be02be
marawan-elsayed-elmadawy <121683924+marawan-elsayed-elmadawy@users.noreply.github.com>
2022-12-30 18:35:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Project 1
+         // write a C# program to write the prime numbers from ni to n2.
+         //The Input: n1 and n2
+         //The Output: The list of the prime numbers from ni to n2
+         //Example: For the input numbers: n1 = 5 and n2 = 18, the output should be
+         // 5 1/7 //11 //13 //17
+         Console.WriteLine("enter n1");
+         int n1 = int.Parse(Console.ReadLine());
+         Console.WriteLine("enter n2");
+         int n2 = int.Parse(Console.ReadLine());
+         Console.WriteLine("the prime numbers between {0} and {1}", n1, n2);
+         for (int i = n1; i <= n2; i++)
+         {
+             int counter = 0;
+             for (int j = 2; j <= i / 2; j++)
+                 if (i % j == 0)
+                 { }
+             counter++;
+             break;
+
+             if (counter == 0 && i != 1)
+             {
+                 Console.WriteLine("{0}", i);
+             }
+
+
+            }
+        }
+    }
+}
+        
+    
+
